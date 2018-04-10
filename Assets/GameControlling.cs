f253768f6079accf740e913e6b001b21215d6849
Clément Pirelli <clepirelli@gmail.com>
2018-04-10 11:19:56 +0200

    music + score
    ye

---
+++ Assets/GameControlling.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MusicInstructions))]
+public class GameControlling : MonoBehaviour {
+    public AudioClip music;
+    private Pair<MusicInstructions.DanceMove, float>[] instructions = new Pair<MusicInstructions.DanceMove, float>[2];
+    // Use this for initialization
+    void Start()
+    {
+        instructions[0] = new Pair<MusicInstructions.DanceMove, float>(MusicInstructions.DanceMove.BothArmsUp, 1f);
+        instructions[1] = new Pair<MusicInstructions.DanceMove, float>(MusicInstructions.DanceMove.LeftArmUp, 2f);
+        GetComponent<MusicInstructions>().SetMusic(music, instructions);
+    }
+}
---
+++ Assets/MusicInstructions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicInstructions : MonoBehaviour {
+    private Pair<DanceMove, float>[] pairs;
+    private float accumulatedTime = 0f;
+    public AudioSource musicSource;
+    private int lastPairIndex = 0;
+    private bool finished = false;
+    public enum DanceMove
+    {
+        LeftArmUp,
+        RightArmUp,
+        BothArmsUp
+    }
+
+	// Use this for initialization
+	void Start () {
+        if (pairs.Length == 0)
+        {
+            finished = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(finished == false)
+        {
+            accumulatedTime += Time.deltaTime;
+            if(accumulatedTime >= pairs[lastPairIndex].secondValue)
+            {
+                Debug.Log(pairs[lastPairIndex].firstValue);
+                lastPairIndex++;
+                if(pairs.Length <= lastPairIndex)
+                {
+                    finished = true;
+                    lastPairIndex = 0;
+                    accumulatedTime = 0f;
+                }
+            }
+        }
+	}
+
+    public void SetMusic(AudioClip givenMusic, Pair<DanceMove, float>[] givenPairs)
+    {
+        musicSource.clip = givenMusic;
+        musicSource.Play();
+        pairs = givenPairs;
+        finished = false;
+    }
+}
