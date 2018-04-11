f2ee21e2826bb08c4f78e127e9f7cb3688bb2bf2
Matthias Jannidis <matthias.jannidis@gmail.com>
2018-04-11 11:45:16 +0200

    Actually did the thing that makes scoring work and also you can play game now why ouuood

--- Assets/Scripts/GameControlling.cs
+++ Assets/Scripts/GameControlling.cs
@@ -8,20 +8,26 @@ public class GameControlling : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        DanceMove temp;
-        temp.instructionImageIndex = (int)MusicInstructions.DanceMoveEnum.LeftArmUp;
-        temp.LeftArmPosition = 1f;
-        temp.LeftLegPosition = -1f;
-        temp.RightArmPosition = -1f;
-        temp.RightLegPosition = -1f;
-        instructions[0] = new Pair<DanceMove, float>(temp, 1f);
-        DanceMove temp2;
-        temp2.instructionImageIndex = (int)MusicInstructions.DanceMoveEnum.RightArmUp;
-        temp2.LeftArmPosition = -1f;
-        temp2.LeftLegPosition = -1f;
-        temp2.RightArmPosition = 1f;
-        temp2.RightLegPosition = -1f;
-        instructions[1] = new Pair<DanceMove, float>(temp2, 2f);
+        DanceMove leftArmUp;
+        leftArmUp.instructionImageIndex = (int)MusicInstructions.DanceMoveEnum.LeftArmUp;
+        leftArmUp.LeftArmPosition = 2f;
+        leftArmUp.LeftLegPosition = -1f;
+        leftArmUp.RightArmPosition = 0f;
+        leftArmUp.RightLegPosition = -1f;
+        instructions[0] = new Pair<DanceMove, float>(leftArmUp, 1f);
+        DanceMove rightArmUp;
+        rightArmUp.instructionImageIndex = (int)MusicInstructions.DanceMoveEnum.RightArmUp;
+        rightArmUp.LeftArmPosition = 0f;
+        rightArmUp.LeftLegPosition = -1f;
+        rightArmUp.RightArmPosition = 2f;
+        rightArmUp.RightLegPosition = -1f;
+        instructions[1] = new Pair<DanceMove, float>(rightArmUp, 2f);
         GetComponent<MusicInstructions>().SetMusic(music, instructions);
+    }
+    void Update()
+    {
+
+        Debug.Log("LeftArm: " + GameObject.FindGameObjectWithTag("LeftArm").GetComponent<LimbMovement>().GetLimbState() + "RightArm: " + GameObject.FindGameObjectWithTag("RightArm").GetComponent<LimbMovement>().GetLimbState());
+        //Debug.Log("LeftLeg: " + GameObject.FindGameObjectWithTag("LeftLeg").GetComponent<LimbMovement>().GetLimbState() + "RightLeg: " + GameObject.FindGameObjectWithTag("RightLeg").GetComponent<LimbMovement>().GetLimbState());
     }
 }
--- Assets/Scripts/LimbMovement.cs
+++ Assets/Scripts/LimbMovement.cs
@@ -13,6 +13,23 @@ public class LimbMovement : MonoBehaviour {
     }
 	
 	void Update () {
-            transform.rotation = transform.rotation = Quaternion.Euler(0, 0, Input.GetAxis(joystickAxis) * maxRotation + startRotation);
+        transform.rotation = transform.rotation = Quaternion.Euler(0, 0, Input.GetAxis(joystickAxis) * maxRotation + startRotation);
     }
+
+    public int GetLimbState()
+    {
+        int tempLimbState = -1;
+        if (Mathf.DeltaAngle(transform.rotation.eulerAngles.z, startRotation) < 40 && Mathf.DeltaAngle(transform.rotation.eulerAngles.z, startRotation) > -40)
+        {
+            tempLimbState = 1;
+        } else if (Mathf.DeltaAngle(transform.rotation.eulerAngles.z, startRotation) >= 40)
+        {
+            tempLimbState = 0;
+        } else if (Mathf.DeltaAngle(transform.rotation.eulerAngles.z, startRotation) <= -40)
+        {
+            tempLimbState = 2;
+        }
+        if (gameObject.tag == "RightArm") tempLimbState -= 2; tempLimbState = Mathf.Abs(tempLimbState);
+        return tempLimbState;
+    }//TODO: get number based on rotation
 }
