0b56ae58a8fdc191a6a9a392cdf3d0f7167af609
Clément Pirelli <clepirelli@gmail.com>
2018-04-12 13:05:12 +0200

    added leaderboard (broken)

--- Assets/Scripts/ScoringSystem.cs
+++ Assets/Scripts/ScoringSystem.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoringSystem : MonoBehaviour {
+    public static string firstPlayerScoreKey = "FirstPlayerScore";
+    public static string secondPlayerScoreKey = "SecondPlayerScore";
     int firstPlayerScore = 0;
     int secondPlayerScore = 0;
     public Text firstPlayerScoreDisplay;
@@ -13,12 +15,14 @@ public class ScoringSystem : MonoBehaviour {
     {
         firstPlayerScore += scoreAmount;
         firstPlayerScoreDisplay.text = firstPlayerScore.ToString();
+        PlayerPrefs.SetInt(firstPlayerScoreKey, firstPlayerScore);
     }
 
     public void AddSecondPlayerScore(int scoreAmount)
     {
         secondPlayerScore += scoreAmount;
         secondPlayerScoreDisplay.text = secondPlayerScore.ToString();
+        PlayerPrefs.SetInt(secondPlayerScoreKey, secondPlayerScore);
     }
 
     public int GetFirstPlayerScore() { return firstPlayerScore; }
