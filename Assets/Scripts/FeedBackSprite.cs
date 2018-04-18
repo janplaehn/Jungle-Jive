c9728b59fcd9070d6d44f2c3271d256f413fa51b
Krzemisir <38097972+Krzemisir@users.noreply.github.com>
2018-04-18 15:57:06 +0200

    Revert "Revert "Merge branch 'master' into krzesimir""
    This reverts commit f2b2184a129ded4c5b61cd470506534182fd70bb.

---
+++ Assets/Scripts/FeedBackSprite.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FeedBackSprite : MonoBehaviour {
+    private Vector2 direction = Vector2.zero;
+    public float speed = 2f;
+    public float lifeTime = 1f;
+
+	// Use this for initialization
+	void Start () {
+        direction.y = -1f;
+        direction.Normalize();
+        Destroy(gameObject, lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(direction.x * speed * Time.deltaTime, direction.y * speed * Time.deltaTime, 0);
+	}
+
+    public void SetSprite(Sprite givenSprite)
+    {
+        GetComponent<SpriteRenderer>().sprite = givenSprite;
+    }
+}
---
+++ Assets/Scripts/MusicInstructions.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicInstructions : MonoBehaviour {
+    private Pair<DanceMove, float>[] timingPairs;
+    public Sprite voidSprite;
+    public Sprite[] instructionImageArray;
+    public Image instruction;
+    public Image timing;
+    //public Image timing;
+    public Image nextInstruction;
+    private float accumulatedTime = 0f;
+    public AudioSource musicSource;
+    private ScoringSystem scoringSystem;
+    private InputCheck inputCheck;
+    private DanceMove lastMove;
+    private int lastPairIndex = 0;
+    private bool finished = false;
+    private bool moveRatedP1 = false;
+    private bool moveRatedP2 = false;
+    public Sprite timingSprite;
+
+    public float instructionTime;
+    private float errorMargin = 0.2f;
+    private float scaleTiming = 1;
+    public enum DanceMoveEnum
+    {
+        LeftArmUp,
+        RightArmUp,
+        BothArmsUp,
+        BothArmsDown,
+        SplitArmsUp,
+        SplitArmsDown,
+        LeftArmLeftLegUp,
+        RightArmRightLegUp,
+    }
+	// Use this for initialization
+	void Start () {
+        lastMove = timingPairs[0].firstValue;
+        nextInstruction.sprite = instructionImageArray[lastMove.instructionImageIndex];
+        inputCheck = GetComponent<InputCheck>();
+        scoringSystem = GetComponent<ScoringSystem>();
+        if (timingPairs.Length == 0)
+        {
+            finished = true;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(finished == false)
+        {
+            accumulatedTime += Time.deltaTime;
+
+
+
+            if (accumulatedTime <= instructionTime)
+            {
+                animateTiming(timingPairs[lastPairIndex].secondValue, accumulatedTime);
+                if (inputCheck.CheckLimbs(lastMove, InputCheck.Players.PlayerOne) == 1 && moveRatedP1 == false)
+                {
+                    if (accumulatedTime > timingPairs[lastPairIndex].secondValue - errorMargin && accumulatedTime < timingPairs[lastPairIndex].secondValue + errorMargin)
+                    {
+                        scoringSystem.AddFirstPlayerScore(inputCheck.CheckScore(lastMove, 2, InputCheck.Players.PlayerOne), inputCheck.GetMaxScore(lastMove));
+                        
+                    } else
+                    {
+                        scoringSystem.AddFirstPlayerScore(inputCheck.CheckScore(lastMove, 1, InputCheck.Players.PlayerOne), inputCheck.GetMaxScore(lastMove));
+                    }
+                    moveRatedP1 = true;
+                }
+                if (inputCheck.CheckLimbs(lastMove, InputCheck.Players.PlayerTwo) == 1 && moveRatedP2 == false)
+                {
+                    if (accumulatedTime > timingPairs[lastPairIndex].secondValue - errorMargin && accumulatedTime < timingPairs[lastPairIndex].secondValue + errorMargin)
+                    {
+                        scoringSystem.AddSecondPlayerScore(inputCheck.CheckScore(lastMove, 2, InputCheck.Players.PlayerTwo), inputCheck.GetMaxScore(lastMove));
+
+                    }
+                    else
+                    {
+                        scoringSystem.AddSecondPlayerScore(inputCheck.CheckScore(lastMove, 1, InputCheck.Players.PlayerTwo), inputCheck.GetMaxScore(lastMove));
+                    }
+                    moveRatedP2 = true;
+                }
+                if (accumulatedTime >= timingPairs[lastPairIndex].secondValue) timing.sprite = voidSprite;
+            } else
+            {
+
+                if (moveRatedP1 == false) scoringSystem.AddFirstPlayerScore(inputCheck.CheckScore(lastMove, 1, InputCheck.Players.PlayerOne), inputCheck.GetMaxScore(lastMove));
+                if (moveRatedP2 == false) scoringSystem.AddSecondPlayerScore(inputCheck.CheckScore(lastMove, 1, InputCheck.Players.PlayerTwo), inputCheck.GetMaxScore(lastMove));
+                lastPairIndex++;
+                if (timingPairs.Length <= lastPairIndex)
+                {
+                    finished = true;
+                    instruction.sprite = voidSprite;
+                    nextInstruction.sprite = voidSprite;
+                    lastPairIndex = 0;
+                    accumulatedTime = 0f;
+                    GetComponent<GameControlling>().GameOver();
+                    return;
+                }
+
+                moveRatedP1 = false;
+                moveRatedP2 = false;
+                accumulatedTime = 0f;
+                instructionTime = timingPairs[lastPairIndex].secondValue + 1f;
+                instruction.sprite = instructionImageArray[timingPairs[lastPairIndex].firstValue.instructionImageIndex];
+                timing.sprite = timingSprite;
+                timing.rectTransform.localScale = new Vector3(scaleTiming, scaleTiming, 1);
+                if (lastPairIndex + 1 <= timingPairs.Length - 1)
+                {
+                    nextInstruction.sprite = instructionImageArray[timingPairs[lastPairIndex + 1].firstValue.instructionImageIndex];
+                }
+                else
+                {
+                    nextInstruction.sprite = voidSprite;
+                }
+                lastMove = timingPairs[lastPairIndex].firstValue;
+            }
+        }
+	}
+
+    public void SetMusic(AudioClip givenMusic, Pair<DanceMove, float>[] givenPairs)
+    {
+        musicSource.clip = givenMusic;
+        musicSource.Play();
+        timingPairs = givenPairs;
+        finished = false;
+    }
+
+    void animateTiming (float perfectTime, float accumulatedTime)
+    {
+        timing.rectTransform.localScale = new Vector3(1 + (2 * (perfectTime - accumulatedTime)) / perfectTime, 1 + (2 * (perfectTime - accumulatedTime)) / perfectTime, 0);
+        if ((1 + (2 * (perfectTime - accumulatedTime)) / perfectTime) <= 1) {
+            timing.sprite = voidSprite;
+        }
+    }
+}
