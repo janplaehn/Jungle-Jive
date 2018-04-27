81fb1f105cd340f10e9c60a6f78e6d6986377c63
janplaehn <janplaehn@yahoo.de>
2018-04-27 14:12:38 +0200

    Chicken

---
+++ Assets/BirdControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdControl : MonoBehaviour {
+
+    public float movementRadius;
+    private int flightdirection;
+
+	void Start () {
+	    	
+	}
+	
+	void Update () {
+        transform.position += Vector3.right * movementRadius * Time.deltaTime;
+	}
+}
