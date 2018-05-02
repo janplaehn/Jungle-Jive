962fba435fde37b6d2359b2d78b32118f287bb2a
Clément Pirelli <clepirelli@gmail.com>
2018-05-02 15:20:04 +0200

    changed monkey render order

--- Assets/Scripts/Items/Mushroom.cs
+++ Assets/Scripts/Items/Mushroom.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Mushroom : MonoBehaviour {
     public static Material firstPlayerDizzy;
     public static Material secondPlayerDizzy;
