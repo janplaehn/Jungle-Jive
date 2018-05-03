5d2a1f434b38137cf3c5cfd2396fea976ef23f36
janplaehn <janplaehn@yahoo.de>
2018-05-03 13:48:10 +0200

    big mirror update ye

---
+++ Assets/MirrorEffect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorEffect : MonoBehaviour {
+
+    public float effectTime = 4;
+    public Transform mirror;
+
+    private const float mirrorOffset = 5.66f;
+    private GameObject playerOneSpawn;
+    private GameObject playerTwoSpawn;
+    private GameObject mirrorInstanceOne;
+    private GameObject mirrorInstanceTwo;
+
+    private void Start() {
+        playerOneSpawn = GameObject.Find("playerOneSpawn");
+        playerTwoSpawn = GameObject.Find("playerTwoSpawn");
+    }
+
+    public void ActivateMirror(string tag) {
+        GameObject.FindGameObjectWithTag(tag).GetComponent<MirrorMovement>().isMirrored = true;
+        StartCoroutine(DeactivateMirror(tag));
+        if (tag == "Player1") {
+            mirrorInstanceOne = Instantiate(mirror, playerOneSpawn.transform.position + Vector3.down * mirrorOffset, Quaternion.identity).gameObject;
+        }
+        else if (tag == "Player2") {
+            mirrorInstanceTwo = Instantiate(mirror, playerTwoSpawn.transform.position + Vector3.down * mirrorOffset, Quaternion.identity).gameObject;
+        }
+
+    }
+
+    IEnumerator DeactivateMirror(string tag) {
+        yield return new WaitForSeconds(effectTime);
+        GameObject.FindGameObjectWithTag(tag).GetComponent<MirrorMovement>().isMirrored = false;
+        if (tag == "Player1") {
+            Destroy(mirrorInstanceOne);
+        }
+        else if (tag == "Player2") {
+            Destroy(mirrorInstanceTwo);
+        }
+    }
+}
---
+++ Assets/MirrorMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorMovement : MonoBehaviour {
+
+    [HideInInspector] public bool isMirrored = false;
+
+	void Start () {
+        isMirrored = false;
+	}
+	
+	void Update () {
+        SetMirror();
+	}
+
+    void SetMirror() {
+        if (isMirrored) {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
