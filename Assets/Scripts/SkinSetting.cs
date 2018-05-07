c1d9d04b09e0557ac32592f5214a7146121a7d35
Clément Pirelli <clepirelli@gmail.com>
2018-05-07 15:54:42 +0200

    ADDED PLAYER PREFABS BEING FETCHED AND SHIT YEEEEHAWWWW

--- Assets/Scripts/SkinSetting.cs
+++ Assets/Scripts/SkinSetting.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SkinSetting : MonoBehaviour {
-    public GameObject[] playerOnePrefabs;
-    public GameObject[] playerTwoPrefabs;
+    [HideInInspector] public List<GameObject> playerOnePrefabs = new List<GameObject>();
+    [HideInInspector] public List<GameObject> playerTwoPrefabs = new List<GameObject>();
     private int playerOneIndex;
     private int playerTwoIndex;
 
@@ -21,7 +21,7 @@ public class SkinSetting : MonoBehaviour {
         //PlayerPrefs.DeleteAll();
         playerOneIndex = PlayerPrefs.GetInt("FirstPlayerSkin", 0);
         PlayerPrefs.SetInt("FirstPlayerSkin", playerOneIndex);
-        playerTwoIndex = PlayerPrefs.GetInt("SecondPlayerSkin", playerTwoPrefabs.Length - 1);
+        playerTwoIndex = PlayerPrefs.GetInt("SecondPlayerSkin", playerTwoPrefabs.Count - 1);
         PlayerPrefs.SetInt("SecondPlayerSkin", playerTwoIndex);
         playerOne = Instantiate(playerOnePrefabs[playerOneIndex], playerOneSpawn.transform.position, Quaternion.identity);
         if (showSecondPlayer == true) playerTwo = Instantiate(playerTwoPrefabs[playerTwoIndex], playerTwoSpawn.transform.position, Quaternion.identity);
@@ -31,10 +31,27 @@ public class SkinSetting : MonoBehaviour {
         }
     }
 
+    private void GetSkins()
+    {
+        GameObject[] temp = Resources.LoadAll("PlayerPrefabs", typeof(GameObject)) as GameObject[];
+        foreach(GameObject t in temp)
+        {
+            if(t.tag == "Player1")
+            {
+                playerOnePrefabs.Add(t);
+            }
+            if(t.tag == "Player2")
+            {
+                playerTwoPrefabs.Add(t);
+            }
+        }
+    }
+
+
     public void ChangePlayerOneSkin(int deltaIndex)
     {
-        if (playerOneIndex + deltaIndex >= playerOnePrefabs.Length) deltaIndex = -playerOneIndex;
-        if (playerOneIndex + deltaIndex < 0) deltaIndex = playerOnePrefabs.Length - playerOneIndex - 1;
+        if (playerOneIndex + deltaIndex >= playerOnePrefabs.Count) deltaIndex = -playerOneIndex;
+        if (playerOneIndex + deltaIndex < 0) deltaIndex = playerOnePrefabs.Count - playerOneIndex - 1;
         playerOneIndex += deltaIndex;
         PlayerPrefs.SetInt("FirstPlayerSkin", playerOneIndex);
         Destroy(playerOne);
@@ -46,8 +63,8 @@ public class SkinSetting : MonoBehaviour {
 
     public void ChangePlayerTwoSkin(int deltaIndex)
     {
-        if (playerTwoIndex + deltaIndex >= playerTwoPrefabs.Length) deltaIndex = -playerTwoIndex;
-        if (playerTwoIndex + deltaIndex < 0) deltaIndex = playerTwoPrefabs.Length - playerTwoIndex - 1;
+        if (playerTwoIndex + deltaIndex >= playerTwoPrefabs.Count) deltaIndex = -playerTwoIndex;
+        if (playerTwoIndex + deltaIndex < 0) deltaIndex = playerTwoPrefabs.Count - playerTwoIndex - 1;
         playerTwoIndex += deltaIndex;
         PlayerPrefs.SetInt("SecondPlayerSkin", playerTwoIndex);
         Destroy(playerTwo);
