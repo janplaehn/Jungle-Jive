949d1d47ed165a783f323da7ab48eaac9b914a86
janplaehn <janplaehn@yahoo.de>
2018-05-09 13:35:57 +0200

    Added Scorebar

---
+++ Assets/ScorebarController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScorebarController : MonoBehaviour {
+
+    private Image image;
+    private float firstPlayerScore;
+    private float secondPlayerScore;
+
+    void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	void Update () {
+        CalculateScoreBalance();	
+	}
+
+    void CalculateScoreBalance() {
+        firstPlayerScore = PlayerPrefs.GetInt("FirstPlayerScore", 0);
+        secondPlayerScore = PlayerPrefs.GetInt("SecondPlayerScore", 0);
+        if (firstPlayerScore + secondPlayerScore != 0) {
+            image.fillAmount = firstPlayerScore / (firstPlayerScore + secondPlayerScore);
+        }
+        else {
+            image.fillAmount = 0.5f;
+        }
+    }
+}
