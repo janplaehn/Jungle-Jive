1e1bb9b4a74f1bb83456b332fe3a937fceb5dfdb
Matthias Jannidis <matthias.jannidis@gmail.com>
2018-05-14 15:35:08 +0200

    more prefab changes

--- Assets/Scripts/DanceMove.cs
+++ Assets/Scripts/DanceMove.cs
@@ -1,4 +1,8 @@
-﻿[System.Serializable]
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
 public class DanceMove : UnityEngine.MonoBehaviour
 {
     public enum LimbState
@@ -13,10 +17,11 @@ public class DanceMove : UnityEngine.MonoBehaviour
     public LimbState LeftArmPosition;
     public LimbState RightLegPosition;
     public LimbState LeftLegPosition;
+    public Sprite moveInstruction;
     public MusicInstructions.DanceMoveEnum instructionImageIndex;
 }
 
-public struct TempMove
+public struct TempMove 
 {
     public enum LimbState
     {
