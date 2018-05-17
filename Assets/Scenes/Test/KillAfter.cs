c2c4cd76f0fff76156ae960642eac1ece96adda0
Clément Pirelli <clepirelli@gmail.com>
2018-05-17 15:12:57 +0200

    dust particles hjajdajkodakfngjknjnwjaknfmgvbmnTmfkLéAKMKhnKäFDKLckynmvk

---
+++ Assets/Scenes/Test/KillAfter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillAfter : MonoBehaviour {
+    public float killTimer;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, killTimer);
+	}
+}
