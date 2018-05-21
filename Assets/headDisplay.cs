e82457feae31c0db49c7b8c717fe6e4a2e020056
janplaehn <janplaehn@yahoo.de>
2018-05-21 15:30:38 +0200

    Added new Scorebar

---
+++ Assets/headDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class headDisplay : MonoBehaviour {
+
+    public bool isPlayerOne;
+
+    private GameObject player;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (!player) {
+            if (isPlayerOne) {
+                if (GameObject.FindGameObjectWithTag("Head"))
+                    player = GameObject.FindGameObjectWithTag("Head");
+            }
+            else {
+                if (GameObject.FindGameObjectWithTag("HeadP2"))
+                    player = GameObject.FindGameObjectWithTag("HeadP2");
+            }
+        }
+        GetComponent<Image>().sprite = player.GetComponent<SpriteRenderer>().sprite;
+	}
+}
