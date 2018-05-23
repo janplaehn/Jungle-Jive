b152f864cedfcfd8b5f6737f3349351c52df6b1c
Matthias Jannidis <matthias.jannidis@gmail.com>
2018-05-23 11:13:11 +0200

    Go get em tiger

--- Assets/Scripts/ComboManager.cs
+++ Assets/Scripts/ComboManager.cs
@@ -11,6 +11,10 @@ public class ComboManager : MonoBehaviour {
     public GameObject comboP2Image;
     private int comboStreakP1;
     private int comboStreakP2;
+    public GameObject fireP1;
+    public GameObject fireP2;
+    private Animator fireP1Animator;
+    private Animator fireP2Animator;
     [SerializeField] Color invisible;
     [SerializeField] Color noCombo;
     [SerializeField] Color smallCombo;
@@ -25,6 +29,8 @@ public class ComboManager : MonoBehaviour {
         comboP2Image.GetComponent<Image>().color = invisible;
         ScoringSystem.comboMultiplierP1 = 1;
         ScoringSystem.comboMultiplierP1 = 1;
+        fireP1Animator = fireP1.GetComponent<Animator>();
+        fireP2Animator = fireP2.GetComponent<Animator>();
 
 
     }
@@ -35,17 +41,22 @@ public class ComboManager : MonoBehaviour {
         {
             comboP1.GetComponent<Text>().color = smallCombo;
             comboP1Image.GetComponent<Image>().color = smallCombo;
+
         }
         if (comboStreakP1 >= 4)
         {
             ScoringSystem.comboMultiplierP1 = 2;
             comboP1.GetComponent<Text>().color = bigCombo;
             comboP1Image.GetComponent<Image>().color = bigCombo;
+            fireP1.GetComponent<Image>().enabled = true;
+            fireP1Animator.SetBool("FireCombo", true);
         }
         if (comboStreakP1 < 1)
         {
             comboP1.GetComponent<Text>().color = invisible;
             comboP1Image.GetComponent<Image>().color = invisible;
+            fireP1.GetComponent<Image>().enabled = false;
+            fireP1Animator.SetBool("FireCombo", false);
         }
 
 
@@ -59,11 +70,15 @@ public class ComboManager : MonoBehaviour {
             ScoringSystem.comboMultiplierP2 = 2;
             comboP2.GetComponent<Text>().color = bigCombo;
             comboP2Image.GetComponent<Image>().color = bigCombo;
+            fireP2.GetComponent<Image>().enabled = true;
+            fireP2Animator.SetBool("FireCombo", true);
         }
         if (comboStreakP2 < 1)
         {
             comboP2.GetComponent<Text>().color = invisible;
             comboP2Image.GetComponent<Image>().color = invisible;
+            fireP2.GetComponent<Image>().enabled = false;
+            fireP2Animator.SetBool("FireCombo", false);
         }
     }
 
